609270361674b011afcd23047ee58bd0b88dbc9e
EKostinou <vagelis789@gmail.com>
2019-10-19 21:06:27 +0300

    Changed comments and completed Trap class

--- Εργασία Α/src/Food.java
+++ Εργασία Α/src/Food.java
@@ -2,58 +2,81 @@
  * Class that represents every supply
  */
 public class Food {
-	private int id; // id of the Food
+	private int id; // id of the supply
 	private int x; // x coordinate of the supply
 	private int y; // y coordinate of the supply
-	private int points; // point that the player wins when he eats the supply
+	private int points; // points that the player wins when he eats the supply
 
-	//////////////////////////////////
-	// Setters
+	/**
+	 * id setter
+	 * @param id id of the supply
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * x setter
+	 * @param x x coordinate of the supply
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/** 
+	 * y setter
+	 * @param y y coordinate of the supply
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 	
+	/**
+	 * points setter
+	 * @param points points that the player wins when he eats the supply
+	 */
 	public void setPoints(int points) {
 		this.points = points;
 	}
 	
-	// End of setters
-	//////////////////////////////////
-		
-	//////////////////////////////////
-	// Getters
+	/**
+	 * id getter
+	 * @return id of the supply
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
+	/**
+	 * x getter
+	 * @return x coordinate of the supply
+	 */
 	public int getX() {
 		return this.x;
 	}
 	
+	/**
+	 * y getter
+	 * @return y coordinate of the supply
+	 */
 	public int getY() {
 		return this.y;
 	}
 	
+	/**
+	 * points getter
+	 * @return points that the player wins when he eats the supply
+	 */
 	public int getPoints() {
 		return this.points;
 	}
 	
-	// End of getters
-	//////////////////////////////////
 	/**
 	 * Food constructor with each variable as argument
 	 * @param id id of the Food
 	 * @param x x coordinate of the supply
 	 * @param y y coordinate of the supply
-	 * @param points point that the player wins when he eats the supply
+	 * @param points points that the player wins when he eats the supply
 	 */
 	public Food(int id, int x, int y, int points) {
 		this.id = id;
--- Εργασία Α/src/Weapon.java
+++ Εργασία Α/src/Weapon.java
@@ -6,62 +6,90 @@ public class Weapon {
 	private int id; // id of the weapon
 	private int x; // x coordinate of the weapon's position
 	private int y; // y coordinate of the weapon's position
-	private int  playerID; // player's who own's the weapon id
+	private int  playerID; // id of the player who own's the weapon
 	private String type; // type of weapon (pistol, bow, sword)
 	
-	//////////////////////////////////
-	// Setters
+	/**
+	 * id setter
+	 * @param id id of the weapon
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * x setter
+	 * @param x x coordinate of the weapon's position
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/**
+	 * y setter
+	 * @param y y coordinate of the weapon's position
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 	
+	/**
+	 * playerID setter
+	 * @param playerID id of the player who own's the weapon
+	 */
 	public void setPlayerID(int playerID) {
 		this.playerID = playerID;
 	}
 	
-	// type setter (check if we have a correct type)
+	/**
+	 * type setter (check if we have a correct type)
+	 * @param type type of weapon (pistol, bow, sword)
+	 */
 	public void setType(String type) {
 		if(type == "pistol" || type == "bow" || type == "sword") {
 			this.type = type;
 		}
 	}
 	
-	// End of setters
-	//////////////////////////////////
-	
-	//////////////////////////////////
-	// Getters
-	
+	/**
+	 * id getter
+	 * @return id of the weapon
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
+	/**
+	 * x getter
+	 * @return x coordinate of the weapon's position
+	 */
 	public int getX() {
 		return this.x;
 	}
 	
+	/**
+	 * y getter
+	 * @return y coordinate of the weapon's position
+	 */
 	public int getY() {
 		return this.y;
 	}
 	
+	/**
+	 * playerID getter
+	 * @return id of the player who own's the weapon
+	 */
 	public int getPlayerID() {
 		return this.playerID;
 	}
 	
+	/**
+	 * type getter
+	 * @return type of weapon (pistol, bow, sword)
+	 */
 	public String getType() {
 		return this.type;
 	}
-	
-	// End of getters
-	//////////////////////////////////
 	
 	/**
 	 * Weapon constructor with each variable as argument
